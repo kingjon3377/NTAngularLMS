31a968301e2cdd2df00b484d472164b223997184
Jonathan Lovelace <kingjon3377@gmail.com>
2019-05-07 15:22:35 -0400

    Implement admin-dashboard chrome (header and sidebar menu).

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomepageComponent,
+    children: []
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
     children: []
   }
 ];
