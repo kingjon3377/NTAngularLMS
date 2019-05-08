3158f18894ecf30ab13b12d6b64e9bb3189776a7
Jonathan Lovelace <kingjon3377@gmail.com>
2019-05-08 14:32:23 -0400

    AuthorsComponent: Get list of authors from database (for model; not yet reflected in HTML)

--- src/app/admin/authors/authors.component.ts
+++ src/app/admin/authors/authors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-authors',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  authors: any;
 
   ngOnInit() {
+    this.getAllAuthors();
+  }
+
+  getAllAuthors() {
+    this.http.get(environment.api_endpoint + environment.get_all_authors).
+      subscribe(res => {
+        this.authors = res;
+        console.log(res);
+      });
   }
 
 }
