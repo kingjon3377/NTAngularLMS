36c0713b6971fbd534805597154f736753456728
Jonathan Lovelace <kingjon3377@gmail.com>
2019-05-08 14:26:53 -0400

    Add some API endpoint pieces to 'environment' object.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_endpoint: 'http://localhost:8080',
+  get_all_authors: '/authors'
 };
 
 /*
