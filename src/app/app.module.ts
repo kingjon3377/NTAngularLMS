c7a2ebebc4bf9979b19ad989d1fbc7a5bbe20f9e
Jonathan Lovelace <kingjon3377@gmail.com>
2019-05-09 20:30:04 -0400

    Add ng-bootstrap dependency.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { CopiesComponent } from './librarian/copies/copies.component';
 import { LoginComponent } from './admin/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    NgbModule, // TODO: just import the modules we use
     AppRoutingModule
   ],
   providers: [],
