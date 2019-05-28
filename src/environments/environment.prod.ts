be14384a5d47747dd629b82020bf71c37d6bf52a
Salem <SOzaki19@cornellcollege.edu>
2019-05-28 16:25:56 -0400

    create production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  api_endpoint: 'URL:PORT',
+  get_all_publishers: '/publishers',
+  get_publisher: '/publisher/',
+  get_all_books: '/books',
+  get_book: '/book/',
+  admin_endpoint: '',
+  get_all_borrowers: '/borrowers',
+  borrower_api: '/borrower/',
+  book_api: '/book/',
+  branch_api: '/branch/',
+  deleteAuthor: '/deleteAuthor/',
+  get_all_loans: '/loans',
+  get_all_authors: '/authors',
+  single_author: '/author',
+  get_all_branches: '/branches',
+  single_branch: '/branch'
 };
